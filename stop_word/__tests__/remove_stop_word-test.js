765d5ba6e6097435718cb91412881895e766b737
Kaue Miyauti Machado <kaumac@gmail.com>
2015-11-02 18:09:39 -0200

    stop word script done and passing tests

--- stop_word/__tests__/remove_stop_word-test.js
+++ stop_word/__tests__/remove_stop_word-test.js
@@ -6,6 +6,6 @@ describe('removeStopWords', function() {
    var removeStopWords = require('../remove_stop_words');
    var stopWordList = require('../stop_word_list');
    var text = "Sometimes when you innovate, you make mistakes. It is best to admit them quickly, and get on with improving your other innovations";
-   expect(removeStopWords(text, stopWordList)).toBe("innovate mistakes admit quickly improving innovations");
+   expect(removeStopWords(text, stopWordList)).toBe("innovate,make,mistakes,admit,quickly,improving,innovations");
  });
 });
--- stop_word/remove_stop_words.js
+++ stop_word/remove_stop_words.js
@@ -1,4 +1,15 @@
 function removeStopWords(text, stopWordList) {
-  return "test string";
+  var splitTextRegExp = /(\b[^\s]+\b)/g;
+  var wordsArray = text.match(splitTextRegExp);
+  var parsedText = [];
+  for (var word of wordsArray) {
+  	// search mathod on JS is a bit messy, I can't do case sensitive searches using variables as strings,
+  	// so I had to convert the word to lovercase here
+  	var isntStopWord = stopWordList.search(word.toLowerCase()) === -1;
+  	if(isntStopWord) {
+  		parsedText.push(word);
+  	}
+  }
+  return parsedText.join();
 }
 module.exports = removeStopWords;
